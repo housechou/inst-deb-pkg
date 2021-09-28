4acf725f7d1b58dc99a381d95eec6d3f76f3ab48
ui-house-chou <house.chou@ui.com>
2021-09-28 16:00:18 +0800

    install neobundle vim plugin manage

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ sudo apt-get install -y dialog
 
 function install_vim () {
     sudo apt-get install -y vim-gtk3
+    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > neobundle.install.sh
+    sh ./neobundle.install.sh
     if [ ! -f ~/.vimrc ];then
         cp ./.vimrc ~
         if [ "$username" = "root" ];then
