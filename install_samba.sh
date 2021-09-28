d464a208b10fd73ced642b0891838d1766e3c2ab
ui-house-chou <house.chou@ui.com>
2021-09-28 14:07:01 +0800

    fix install_samba.sh

--- install_samba.sh
+++ install_samba.sh
@@ -5,31 +5,30 @@
 #SAMBA_DIR=/srv/samba
 #SAMBA_PASS="lovingsamba"
 
+read -p "samba root directory:" SAMBA_DIR
 read -p "New samba username:" SAMBA_USER
 read -s -p "New user password:" SAMBA_PASS
-read -p "samba root directory:" SAMBA_DIR
 
 sudo apt-get install -y samba
 sudo mkdir -p $SAMBA_DIR
 sudo chgrp sambashare $SAMBA_DIR
-for u in $SAMBA_USERS;do
-    if id "$u" &>/dev/null; then
-        sudo usermod -a -G sambashare $u
-    else
-        sudo useradd -M -d $SAMBA_DIR/$u -s /usr/sbin/nologin -G sambashare $u
-    fi
-    sudo mkdir -p $SAMBA_DIR/$u
-    sudo chgrp sambashare $SAMBA_DIR/$u
-    sudo chmod 770 $SAMBA_DIR/$u
-    echo -ne "$SAMBA_PASS\n$SAMBA_PASS\n" | sudo smbpasswd -a -s $u
-    sudo smbpasswd -e $u
-    echo "[$u]
-      path = $SAMBA_DIR/$u
-      browseable = yes
-      read only = no
-      force create mode = 0660
-      force directory mode = 2770
-      valid users = @sambashare" | sudo tee -a /etc/samba/smb.conf
-done
+u=$SAMBA_USER
+if id "$u" &>/dev/null; then
+    sudo usermod -a -G sambashare $u
+else
+    sudo useradd -M -d $SAMBA_DIR/$u -s /usr/sbin/nologin -G sambashare $u
+fi
+sudo mkdir -p $SAMBA_DIR/$u
+sudo chgrp sambashare $SAMBA_DIR/$u
+sudo chmod 770 $SAMBA_DIR/$u
+echo -ne "$SAMBA_PASS\n$SAMBA_PASS\n" | sudo smbpasswd -a -s $u
+sudo smbpasswd -e $u
+echo "[$u]
+  path = $SAMBA_DIR/$u
+  browseable = yes
+  read only = no
+  force create mode = 0660
+  force directory mode = 2770
+  valid users = @sambashare" | sudo tee -a /etc/samba/smb.conf
 sudo systemctl restart smbd
 sudo systemctl restart nmbd
